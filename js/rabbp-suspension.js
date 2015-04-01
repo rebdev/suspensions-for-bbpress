e5c871884446c9f7101318ffe113125dc9cb9769
Razz <superbecc@gmail.com>
2015-04-01 16:39:16 +1000

    Create rabbp-suspension.js

---
+++ js/rabbp-suspension.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function($) {
+
+	// Display message reminding user they are suspended.
+
+	$("<div></div>").attr("id", "suspension_notification")
+					.addClass("alert alert-warning affix-top") // Bootstrap classes
+					.insertBefore("#page")
+					.html("<strong>" + rabbp_suspension_script_vars.message + "<br />Your suspension ends " + rabbp_suspension_script_vars.expirydate + ".</strong>")
+					.slideDown();
+
+
+});
