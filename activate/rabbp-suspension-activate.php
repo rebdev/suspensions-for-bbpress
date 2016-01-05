9be91eaa4beb96994e5c08010e5d9d816d0dd05f
Razz <superbecc@gmail.com>
2016-01-05 15:25:10 +1000

    Update rabbp-suspension-activate.php

--- activate/rabbp-suspension-activate.php
+++ activate/rabbp-suspension-activate.php
@@ -85,7 +85,7 @@ function rabbp_suspension_setup_database_table() {
 		$sql = "CREATE TABLE $table_name (
 				id mediumint(9) NOT NULL AUTO_INCREMENT,
 				name tinytext NOT NULL,	
-				user_id mediumint(9) NOT NULL,		
+				user_id mediumint(9) NOT NULL UNIQUE,		
 				time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
 				suspended_until datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,	
 				ordinary_bbp_roles text NOT NULL,		
